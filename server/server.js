e1157905e5e6c4a5818acc5917a4825f5d1d1cc4
StephenWenceslao <stephenwenceslao@gmail.com>
2022-04-05 22:32:20 +0800

    Switched to new folder structure. Needs nodemon and concurrently globally installed now

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import words from './words.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
